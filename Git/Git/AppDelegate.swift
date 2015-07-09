18bb35828194e9334d105a98e995a4362cc25bbe
Konstantin Sukharev <poed@me.com>
2015-07-09 20:20:26 +0300

    Add files

---
+++ Git/Git/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Git
+//
+//  Created by Konstantin Sukharev on 09/07/15.
+//  Copyright (c) 2015 Konstantin Sukharev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Git/Git/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Konstantin Sukharev on 09/07/15.
+//  Copyright (c) 2015 Konstantin Sukharev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
